eb2ef8388b812d68ebad1bff7f7ab2ad12b55538
daniela <dggtn97@gmail.com>
2024-11-18 13:33:58 -0300

    modifico clase SerieRepository

--- src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
+++ src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
@@ -1,7 +1,20 @@
 package com.aluracursos.screenmatch.repository;
 
+import com.aluracursos.screenmatch.model.Categoria;
 import com.aluracursos.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface SerieRepository extends JpaRepository<Serie,Long> {
+    Optional<Serie> findTituloContainingIgnoreCase(String nombreSerie);
+
+    List<Serie> findTop5ByEvaluacionDesc();
+
+    List<Serie>findByGenero(Categoria categoria);
+
+    @Query("select s from Serie s WHERE s.totalTemporadas<=: totalTemporadas AND")
+    List<Serie>seriesPorTemporadaYEvaluacion(int totalTemporadas,double evaluaciones)
 }
