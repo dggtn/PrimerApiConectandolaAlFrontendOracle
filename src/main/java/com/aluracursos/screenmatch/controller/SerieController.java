e8163d5d540e9e3eee63d91ef585e8866b0267c8
daniela <dggtn97@gmail.com>
2024-11-18 14:31:47 -0300

    se creó el método convierteDatos()

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,28 +1,37 @@
 package com.aluracursos.screenmatch.controller;
 
 import com.aluracursos.screenmatch.dto.SerieDTO;
-import com.aluracursos.screenmatch.model.Serie;
 import com.aluracursos.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
     @Autowired
     private SerieService servicio;
 
-    @GetMapping("/series")
-    public List<SerieDTO>obtenerTodasLasSeries(){
+    @GetMapping()
+    public List<SerieDTO> obtenerTodasLasSeries(){
         return servicio.obtenerTodasLasSeries();
     }
 
-    @GetMapping("/series/top5")
-    public List<Serie> obtenerTop5(){
+    @GetMapping("/top5")
+    public List<SerieDTO> obtenerTop5(){
         return servicio.obtenerTop5();
+    }
+    @GetMapping("/lanzamientos")
+    public List<SerieDTO> obtenerLanzamientosMasRecientes(){
+        return servicio.obtenerLanzamientosMasRecientes();
+    }
 
+    @GetMapping("/{id}")
+    public SerieDTO obtenerPorId(@PathVariable Long id){
+        return servicio.obtenerPorId(id);
     }
 
 }
