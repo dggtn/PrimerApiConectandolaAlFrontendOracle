a163d3507ec4bfe66c3c756fd5f93e2f38988415
daniela <dggtn97@gmail.com>
2024-11-18 14:56:18 -0300

    modifico seriesDTO

--- src/main/java/com/aluracursos/screenmatch/dto/SerieDTO.java
+++ src/main/java/com/aluracursos/screenmatch/dto/SerieDTO.java
@@ -1,9 +1,9 @@
 package com.aluracursos.screenmatch.dto;
 
 import com.aluracursos.screenmatch.model.Categoria;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 
-public record SerieDTO(String titulo, Integer totalTemporadas,Double evaluacion, String poster, Categoria genero,String actores,String sinopsis) {
+
+public record SerieDTO(long id,String titulo, Integer totalTemporadas,Double evaluacion, String poster, Categoria genero,String actores,String sinopsis) {
+
 
 }
